0149367f7490b047d76a55aaaefef804cff5a29f
earslan <earslan007@gmail.com>
2015-05-04 22:39:13 -0400

    Updated matlab function analyzeAndEvaluate.m

--- matlab/analyzeAndEvaluate.m
+++ matlab/analyzeAndEvaluate.m
@@ -1,9 +1,9 @@
-function [final,val] = analyzeAndEvaluate(folderID, targetThroughput, trialNumber, sampleValues, output_dir )
+function [trainingPcp,estimatedThroughput, accuracy] = analyzeAndEvaluate(folderID, targetThroughput, trialNumber, sampleValues, output_dir )
     testCount = round(trialNumber*0.7);
     trainingCount = trialNumber - testCount;
     testTrials = linspace(0,testCount-1,testCount);
     trainingTrials =linspace(testCount,testCount+trainingCount-1,trainingCount);
-    [pcp,estimatedThroughput] = main(folderID, targetThroughput, testTrials, sampleValues, output_dir,[],0 );
-    main(folderID, targetThroughput, trainingTrials, sampleValues, output_dir, pcp, estimatedThroughput );
+    [trainingPcp,estimatedThroughput] = main(folderID, targetThroughput, testTrials, sampleValues, output_dir,[],0 );
+    [~, accuracy] = main(folderID, targetThroughput, trainingTrials, sampleValues, output_dir, trainingPcp, estimatedThroughput );
 end
 
